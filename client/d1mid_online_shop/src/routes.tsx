b4fac1aaa7de9fe0344f89b5ca5049cdbb746f2d
d1mid <dsmsteam@mail.ru>
2025-11-19 15:01:49 +0300

    Add base routing

---
+++ client/d1mid_online_shop/src/routes.tsx
@@ -0,0 +1,42 @@
+import type { ComponentType } from "react";
+import { ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from "./utils/consts";
+import Admin from "./pages/Admin";
+import Shop from "./pages/Shop";
+import Auth from "./pages/Auth";
+import DevicePage from "./pages/DevicePage";
+import Basket from "./pages/Basket";
+
+
+type RouteConfig = {
+  path: string;
+  Component: ComponentType;
+}
+
+export const authRoutes: RouteConfig[] = [
+  {
+    path: ADMIN_ROUTE,
+    Component: Admin,
+  },
+  {
+    path: BASKET_ROUTE,
+    Component: Basket,
+  },
+];
+
+export const publicRoutes : RouteConfig[] = [
+  {
+    path: SHOP_ROUTE,
+    Component: Shop,
+  },
+  {
+    path: LOGIN_ROUTE,
+    Component: Auth,
+  }, {
+    path: REGISTRATION_ROUTE,
+    Component: Auth,
+  },
+  {
+    path: DEVICE_ROUTE + '/:id',
+    Component: DevicePage,
+  },
+];
