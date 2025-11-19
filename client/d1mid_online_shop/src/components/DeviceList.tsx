103df9e508ef9e051f37b75f77cf98753657ac23
d1mid <dsmsteam@mail.ru>
2025-11-20 01:30:04 +0300

    Add Shop, device comps and bars

---
+++ client/d1mid_online_shop/src/components/DeviceList.tsx
@@ -0,0 +1,19 @@
+import { observer } from 'mobx-react-lite'
+import React, { useContext } from 'react'
+import { Context } from '../main'
+import { Row } from 'react-bootstrap';
+import DeviceItem from './DeviceItem';
+
+const DeviceList = observer(() => {
+  const {device} = useContext(Context)!; // Убрать ! после бэка
+
+  return (
+    <Row className='d-flex'>
+        {device.devices.map(device =>
+          <DeviceItem key={device.id} device = {device}/>
+        )}
+    </Row>
+  )
+})
+
+export default DeviceList
--- client/d1mid_online_shop/src/store/DeviceStore.ts
+++ client/d1mid_online_shop/src/store/DeviceStore.ts
@@ -1,16 +1,16 @@
 import { makeAutoObservable } from "mobx";
 
-interface IType {
+export interface IType {
   id: number;
   name: string;
 }
 
-interface IBrand {
+export interface IBrand {
   id: number;
   name: string;
 }
 
-interface IDevice {
+export interface IDevice {
   id: number;
   name: string;
   price: number;
@@ -22,34 +22,53 @@ export default class DeviceStore {
   _types: IType[];
   _brands: IBrand[];
   _devices: IDevice[];
-
+  _selectedType: IType;
+  _selectedBrand: IBrand;
+  
   constructor() {
     this._types =[
       {id: 1, name: 'Холодильники'},
       {id: 2, name: 'Смартфоны'},
+      {id: 3, name: 'Ноутбуки'},
+      {id: 4, name: 'Телевизоры'},
     ];
     this._brands = [
       {id: 1, name: 'Samsung'},
       {id: 2, name: 'Apple'},
+      {id: 3, name: 'Lenovo'},
+      {id: 4, name: 'Asus'},
     ];
     this._devices = [
-      {id: 1, name: 'Iphone 11', price: 25000, rating: 5, img: 'https://placehold.co/600x400?text=iphone+11'},
-      {id: 2, name: 'Iphone 12', price: 26000, rating: 5, img: 'https://placehold.co/600x400?text=iphone+12'},
-      {id: 3, name: 'Iphone 13', price: 27000, rating: 5, img: 'https://placehold.co/600x400?text=iphone+13'},
-      {id: 4, name: 'Iphone 14', price: 28000, rating: 5, img: 'https://placehold.co/600x400?text=iphone+14'},
+      {id: 1, name: 'Iphone 11', price: 25000, rating: 5, img: 'https://placehold.co/140x150?text=iphone+11'},
+      {id: 2, name: 'Iphone 12', price: 26000, rating: 5, img: 'https://placehold.co/140x150?text=iphone+12'},
+      {id: 3, name: 'Iphone 13', price: 27000, rating: 5, img: 'https://placehold.co/140x150?text=iphone+13'},
+      {id: 4, name: 'Iphone 14', price: 28000, rating: 5, img: 'https://placehold.co/140x150?text=iphone+14'},
+      {id: 5, name: 'Iphone 14', price: 28000, rating: 5, img: 'https://placehold.co/140x150?text=iphone+14'},
+      {id: 6, name: 'Iphone 14', price: 28000, rating: 5, img: 'https://placehold.co/140x150?text=iphone+14'},
+      {id: 7, name: 'Iphone 14', price: 28000, rating: 5, img: 'https://placehold.co/140x150?text=iphone+14'},
+      {id: 8, name: 'Iphone 14', price: 28000, rating: 5, img: 'https://placehold.co/140x150?text=iphone+14'},
     ];
+    this._selectedType = {id: 0, name: 'change'};
+    this._selectedBrand = {id: 0, name: 'change'};
     makeAutoObservable(this);
   }
 
-  setTypes (types: IType[]) {
+  setTypes(types: IType[]) {
     this._types = types;
   }
-   setBrands (brands: IBrand[]) {
+   setBrands(brands: IBrand[]) {
     this._brands = brands;
   }
-   setDevice (devices: IDevice[]) {
+   setDevice(devices: IDevice[]) {
     this._devices = devices;
   }
+  setSelectedType(type: IType) {
+    this._selectedType = type;
+  }
+  setSelectedBrand(brand: IBrand) {
+    this._selectedBrand = brand;
+  }
+
 
   get types() {
     return this._types;
@@ -59,5 +78,11 @@ export default class DeviceStore {
   }
    get devices() {
     return this._devices;
+  }
+  get selectedType() {
+    return this._selectedType;
+  }
+  get selectedBrand() {
+    return this._selectedBrand;
   }
 }
